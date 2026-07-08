313ade6f0209b2c9c51e676cd6d88234698a8c5d
Justin Huynh <Justinhuynh801@gmail.com>
2026-07-08 16:40:26 -0600

    worked on Sudokupuzzle class

---
+++ Sudoku Solver/Components/Models/SudokuEntry.cs
@@ -0,0 +1,36 @@
+﻿namespace Sudoku_Solver.Components.Models;
+
+/// <summary>
+/// represents an entry in sudoku puzzle contains the current value (int), which numbers are aligned with it (bool array)
+/// and if it is solvable (bool)
+/// </summary>
+public class SudokuEntry
+{
+    public int? CurrentValue { get; set; } //null means no value yet
+    
+    public bool[]? AlignedValues{ get; set; } // if CurrentValue not null should be empty to save space
+    
+    public bool? Solvable { get; set; }
+
+    public SudokuEntry() {}
+    
+    public SudokuEntry(int currentValue)
+    {
+        CurrentValue = currentValue;
+        AlignedValues = null;
+        Solvable = null;
+    }
+
+    public SudokuEntry(SudokuPuzzle sudokuPuzzle )
+    {
+        CurrentValue = null;
+        // code to fill AlignedValues and set solvable
+    }
+
+    public void UpdateAlignedValue(int insertedValue) // when inserting into puzzle updates correspoinding aligned values
+    {
+        if(AlignedValues is not null){
+            AlignedValues[insertedValue] = true;
+        }
+    }
+}
---
+++ Sudoku Solver/Components/Models/SudokuPuzzle.cs
@@ -0,0 +1,24 @@
+﻿namespace Sudoku_Solver.Components.Models;
+
+public class SudokuPuzzle
+{
+    public SudokuEntry[,] Puzzle { get; }
+
+    public SudokuPuzzle(int size)
+    {
+        Puzzle = new SudokuEntry[size, size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Puzzle[i, j] = new SudokuEntry();
+            }
+        }
+    }
+    
+    // adds sudoku value into puzzle and updates all aligned values
+    public void AddValue(){}
+    
+    
+    
+}
